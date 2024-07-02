166fb25ccddd6aa56ed44e3f97962e9cd24af12c
manisaparajuli <manisaparajuli66@gmail.com>
2024-07-02 15:19:02 +1000

    userModel created

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -14,6 +14,12 @@ const UserSchema = mongoose.Schema({
       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, 
       "Please enter a valid email"
     ]
+  },
+  password: {
+    type: String,
+    required: [true, "Please add a password"],
+    minLength: [6, 'Password must be up to 6 character'],
+    maxLength: [23, "Password must not be more than 23 characters"]
   }
 })
 
