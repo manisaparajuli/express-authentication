8a2beaf94b9e009ac45846c6db81f53cdcc8fc07
manisaparajuli <manisaparajuli66@gmail.com>
2024-07-03 14:38:39 +1000

    user controller added

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,12 +1,38 @@
-const User = require('../models/userModel')
+const asyncHandler = require('express-async-handler')
+const User = require("../models/userModel")
 
-const registerUser = async(req, res) => {
-  if(!req.body.email){
-    res.status(400);
+const registerUser = asyncHandler( async(req, res) => {
+    const{name, email, password} = req.body ;
+    //validation 
+    if(!name || !email || !password){
+      res.status(400)
+      throw new Error("Please fill in all required field")
+    }
+    if(password.length < 6){
+      res.status(400)
+      throw new Error("Password must contain more than 6 character")
+    }
+    //checking if user already exist in DB
+    const user = await User.findOne({email})
+    if(user){
+      res.status(400)
+      throw new Error("Email already registered")
+    }
+    //create new user
+    const newUser = await User.create({name, email, password})
+    if(newUser){
+      const {_id, name, email,bio, photo, phone} = newUser
+      res.status(201).json({
+      _id,name, email, bio, photo, phone 
+      });
+    }else {
+      res.status(400)
+      throw new Error("Invalid user data")
+    }
   }
-  res.send("register User")
-}
+);
+
 
 module.exports = {
-  registerUser
+  registerUser,
 }
