f7c7737086ff0847b5cf872893dc8af7f3d1fef1
manisaparajuli <manisaparajuli66@gmail.com>
2024-07-04 18:31:11 +1000

    login status

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -117,9 +117,25 @@ const getUser = asyncHandler(async(req, res) =>{
   }
 });
 
+
+//Get login status
+const loginStatus = asyncHandler(async(req, res) =>{
+  const token = req.cookies.token;
+  if(!token){
+    return res.json(false);
+  }
+  //verify token
+  const verified = jwt.verify(token, process.env.JWT_SECRET)
+  if(verified){
+    return res.json(true);
+  }
+  return res.json(false);
+})
+
 module.exports = {
   registerUser,
   loginUser,
   logoutUser,
   getUser,
+  loginStatus,
 }
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const {registerUser, loginUser, logoutUser, getUser} = require("../controllers/userController"); 
+const {registerUser, loginUser, logoutUser, getUser, loginStatus} = require("../controllers/userController"); 
 const protect = require('../middleWare/authMiddleware');
 
 
@@ -8,4 +8,5 @@ router.post('/register', registerUser)
 router.post('/login', loginUser)
 router.get('/logout', logoutUser)
 router.get('/getuser',protect, getUser)
+router.get('/loggedin', loginStatus)
 module.exports = router;
